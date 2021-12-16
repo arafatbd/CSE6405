c4a704c15296f488ba91fdac6620aa0de1ccf09a
Md. Alamin Hossain <alamincsebuet10@gmail.com>
2021-12-16 18:40:31 +0600

    1904103011
    1904103011 first assignment

---
+++ 1904103011/SoftwareTestingAssignment/SoftwareTestingAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SoftwareTestingAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial factorial = new Factorial();
+            Console.WriteLine("Enter n:");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(n + "! = " + factorial.GetFactorial(n));
+        }
+    }
+}
